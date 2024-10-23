4bb4923d7a19585c059787a3a3086d7c3e969794
Sidra Noman <sidranoman09@gmail.com>
2024-10-24 01:38:46 +0500

    push my assigment of next js

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className=' bg-gradient-to-r from-purple-500 to-pink-500  flex justify-center items-center h-screen w-{100%}'>
+    <section className=' bg-gradient-to-r from-purple-500 to-pink-500 flex justify-center items-center h-screen w-{100%}'>
     <div className='bg bg-pink-300 rounded-lg shadow-lg p-8  max-w-2xl text-center '>
         <h2 className='text-2xl font-semibold text-black'>This is my Hero Section</h2>
         <p className='mt-4 text-black'> I am Sidra Noman,a passionte full stack developer and On/Off SEO expert.I hold a master's degree in Econimics.I like Typescript,Next js,Tailwind CSS and Reactjs. </p>
       
     </div>
+    <div> 
+      <h1> <section></section></h1>
+    </div>
      </section>
+
   );
 };
 
